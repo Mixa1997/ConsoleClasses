72995ae7d59cec668386c07a794033e7c774d7ce
Mixa1997 <43315299+Mixa1997@users.noreply.github.com>
2021-01-12 13:48:44 +0200

    Add files via upload

---
+++ ConsoleClasses/Numbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClasses
+{
+    class Numbers
+    {
+        private double x;
+        private double y;
+        private double z;
+        public Numbers(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        private double g()
+        {
+            return Math.Sqrt(x * y * z / 2);
+        }
+        private double s()
+        {
+            return (x + y + z);
+        }
+        private double r()
+        {
+            return (x + y + z) / 2;
+        }
+
+        public void SG()
+        {
+            Console.WriteLine("1 число:" + x + " 2 число:" + y + " 3 число:" + z + " Среднее Геометрическое:" + g());
+        }
+
+        public void Summ()
+        {
+            Console.WriteLine("Сумма 3-х чисел:" + s());
+        }
+        public void R()
+        {
+            Console.WriteLine("Разность 3-х чисел: " + r());
+        }
+    }
+}
---
+++ ConsoleClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите число y:");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Введите число z:");
+            double z = double.Parse(Console.ReadLine());
+            Numbers numbers = new Numbers(x, y, z);
+            numbers.SG();
+            numbers.Summ();
+            numbers.R();
+
+            Console.ReadKey();
+        }
+    }
+}
